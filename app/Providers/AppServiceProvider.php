22b8206f7c23445255d558ff9ff580299d594fb5
raivisk2510 <raiviskrums11@gmail.com>
2026-04-27 20:28:56 +0100

    Set default proxy handling to trust all proxies and update environment check for HTTPS enforcement

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+
+
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
+        if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
     }
